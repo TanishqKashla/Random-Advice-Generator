771b97c7996fa453b7d5671a0ca02431131cf244
TanishqKashla <tanishqkashla11@gmail.com>
2024-06-23 01:13:48 +0530

    Added all files

---
+++ src/Advice.jsx
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import { useState } from 'react';
+import "./Advice.css"
+
+export default function Advice(){
+
+
+
+    const [advice, getAdvice] = useState("Start Generating Advices")
+    const [adviceNumber, getAdviceNumber] = useState(0)
+
+    const fetchAdvice = async () => {
+        const response = await axios.get('https://api.adviceslip.com/advice');
+        // advice =  response.data.slip.advice
+        getAdvice(response.data.slip.advice)
+        getAdviceNumber(response.data.slip.id)
+       
+    }
+
+
+
+    return(
+        <div className='container'>
+            <div className='adviceNumber'>
+                ADVICE #{adviceNumber}
+            </div>
+            <div className='advice'>
+                "{advice}"
+            </div>
+            <div className='generateButton' onClick={fetchAdvice}>
+                <img src="./src/assets/dice.png" alt="dice roll icon" />
+            </div>
+        </div>
+    )
+
+}
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Advice from './Advice'
+function App() {
+  return (
+    <div>
+
+      <Advice/>
+    </div>
+  )
+}
+
+export default App
